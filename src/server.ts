97770083985f5c3d4eb66fd01a768b77be3f344b
YoCode35 <ygregoire35@gmail.com>
2025-02-12 11:34:43 +0100

    Ajout des logs pour debug sur Railway

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,13 @@
 import app from "./app";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log("⚡ Chargement des variables d'environnement...");
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
